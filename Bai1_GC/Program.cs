333dc399aef188c8efa1ddd5aae7e7d69670a9a7
vuongtrong2143 <trongtat9@gmail.com>
2026-03-12 12:49:45 +0700

    Them Bai 1

---
+++ Bai1_GC/Program.cs
@@ -0,0 +1,12 @@
+﻿
+System.Console.WriteLine("2415053122143 - Nguyen Vuong Trong");
+List<int> mang5songuyen =  new List<int>();
+mang5songuyen.Add(24);
+mang5songuyen.Add(150);
+mang5songuyen.Add(531);
+mang5songuyen.Add(221);
+mang5songuyen.Add(43);
+foreach (int i in mang5songuyen)
+{
+    Console.WriteLine(i);
+}
