48a1c0e496e1686e3196ad1dbf1afa8d1c1c3fa7
vuongtrong2143 <trongtat9@gmail.com>
2026-03-12 14:49:15 +0700

    Them Bai 3

---
+++ Bai3/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong");
+
+List<int> danhsach = new List<int>() { 24, 150, 53, 122, 143};
+int somax = 0;
+foreach (int item in danhsach)
+{
+    if (item > somax)
+        { somax = item; }
+}
+Console.WriteLine($"So lon nhat trong danh sach: {somax}");
