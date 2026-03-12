c3f1671889300a341fe54fd071322e2649ac7e2a
vuongtrong2143 <trongtat9@gmail.com>
2026-03-12 13:59:56 +0700

    Them Bai2

---
+++ Bai2/Program.cs
@@ -0,0 +1,9 @@
+﻿System.Console.WriteLine("2415053122143 - Nguyen Vuong Trong");
+
+List<int> danhsach = new List<int>() { 1,3,5,7,9 };
+int sum = 0;
+foreach (int i in danhsach)
+{
+    sum += i;
+}
+Console.WriteLine($"Tong la {sum}");
