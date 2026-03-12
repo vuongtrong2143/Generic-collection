fe3700567cb61852a5c33aaaab2c130fbde97848
vuongtrong2143 <trongtat9@gmail.com>
2026-03-12 15:01:08 +0700

    Them bai 4

---
+++ Bai4/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong");
+List<int> danhsach = new List<int>() { 24, 150, 53, 122, 143};
+int dem = 0;
+foreach(int d in danhsach)
+{
+    if (d%2 == 0)
+    {
+        dem++;
+    }
+}
+Console.WriteLine($"So phan tu chan la: {dem}");
