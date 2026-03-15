7319ad68f7e2b3bea696f00f3995266e90658027
vuongtrong2143 <trongtat9@gmail.com>
2026-03-15 14:47:40 +0700

    Them bai 7

---
+++ Bai7/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong");
+
+List<int> danhsach = new List<int>() { 24, 150, 53, 12, 21, 43};
+Console.WriteLine("\nDanh sach ban dau:");
+foreach (int i in danhsach)
+{
+    Console.Write($"{i}" + " ");
+}
+
+danhsach.Sort();
+
+Console.WriteLine("\n\nDanh sach tang dan:");
+foreach (int i in danhsach)
+{
+    Console.Write($"{i}"+" ");
+}
+
+danhsach.Reverse();
+
+Console.WriteLine("\n\nDanh sach giam dan:");
+foreach (int i in danhsach)
+{
+    Console.Write($"{i}" + " ");
+}
