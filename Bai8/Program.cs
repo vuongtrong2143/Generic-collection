5c9233f27d2df8dc87d7919f6e9cbc5b23aacfe1
vuongtrong2143 <trongtat9@gmail.com>
2026-03-15 23:56:40 +0700

    Them bai 8

---
+++ Bai8/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("2415053122143 - Nguyen Vuong Trong");
+
+List<int> danhsach = new List<int>() { 24, 150, 53, 12, 21, 43 };
+Console.WriteLine("\nDanh sach ban dau:");
+foreach (int i in danhsach)
+{
+    Console.Write($"{i}" + " ");
+}
+
+danhsach.Reverse();
+
+Console.WriteLine("\n\nDanh sach dao nguoc:");
+foreach (int i in danhsach)
+{
+    Console.Write($"{i}" + " ");
+}
